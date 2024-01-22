8d600d81be948110bb35d1a911ed52169b29dd39
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-01-22 17:39:25 +0530

    add image and set color then create modular card

---
+++ JayShreeRam/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  JayShreeRam
+//
+//  Created by Bhumika Patel on 22/01/24.
+//
+
+import SwiftUI
+
+struct Card: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Card()
+}
