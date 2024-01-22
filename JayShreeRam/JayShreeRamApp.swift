c37f73f04a493b166eb863e18a9531c66e75460e
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-01-22 10:19:19 +0530

    Initial Commit

---
+++ JayShreeRam/JayShreeRamApp.swift
@@ -0,0 +1,17 @@
+//
+//  JayShreeRamApp.swift
+//  JayShreeRam
+//
+//  Created by Bhumika Patel on 22/01/24.
+//
+
+import SwiftUI
+
+@main
+struct JayShreeRamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
